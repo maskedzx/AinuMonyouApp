2c07db7db50563d416d67b05e5e6f9b51589629e
akikan-alumi <y_r9fj22sdf@live.jp>
2015-12-25 00:23:00 +0900

    miniJson及びEditor変更テストスクリプト追加

---
+++ AinuMonyouApp/Assets/SliderToBox.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SliderToBox : MonoBehaviour {
+
+    [Range(1,10)]
+    public int value;
+
+    [Range(1, 10)]
+    public float fValue;
+
+    [Range(1, 10)]
+    public double dValue;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
