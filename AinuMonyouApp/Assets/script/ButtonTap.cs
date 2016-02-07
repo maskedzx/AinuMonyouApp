ab918f8e361faa27613b4b8e9480c05c94292812
maskedzx <deziyuu@live.jp>
2016-02-08 07:40:26 +0900

    makeのオブジェクト・ボタン･スクリプト追加

---
+++ AinuMonyouApp/Assets/script/ButtonTap.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonTap : MonoBehaviour {
+    [SerializeField]
+    private int buttonNo = 0;
+    [SerializeField]
+    private GameObject partsSpawner;
+    private PartsSpawner ps;
+
+    void Awake(){
+        ps = partsSpawner.GetComponent<PartsSpawner>();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void partsSet(){
+        ps.partsSpawn(buttonNo);
+    }
+
+}
---
+++ AinuMonyouApp/Assets/script/PartsSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PartsSpawner : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] parts;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void partsSpawn(int num){
+        Instantiate(parts[num], transform.localPosition, transform.localRotation);
+    }
+}
