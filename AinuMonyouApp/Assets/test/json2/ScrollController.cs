264441627cb951f069bccebcccd2c4dd3569a0b3
akikan-alumi <y_r9fj22sdf@live.jp>
2016-02-24 03:38:08 +0900

    ログ出力変えただけ

--- AinuMonyouApp/Assets/test/json2/ScrollController.cs
+++ AinuMonyouApp/Assets/test/json2/ScrollController.cs
@@ -38,7 +38,7 @@ public class ScrollController : MonoBehaviour {
 	public void LoadAinu(int i){
 		string str = info[i].Name;
 		appParam _appParam = jsonSystem.Load (info [i].Name);
-        print(_appParam.link);
+        print(_appParam.link.Length);
 	}
 
 	public void DeleteEnterButton(){
