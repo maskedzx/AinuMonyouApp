666ef16c16ae04d0fde55d6c85695ac0ea7be0a0
YuuyaObayashi <deziyuu@live.jp>
2016-02-23 14:46:00 +0900

    SNS連携機能追加

---
+++ AinuMonyouApp/Assets/script/snsTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using SWorker;
+
+public class snsTest : MonoBehaviour {
+    string message = "test";
+    string url = "test";
+    string imagePath = "/storage/emulated/0/Android/data/com.ainumonyou.product/files/screenshot.png";
+
+
+	
+
+	public void testSns () {
+        SocialWorker.PostTwitter(message, url, imagePath);
+	}
+}
