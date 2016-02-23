da34b6287fc31c42621bbf56ce9c24b883b2a59a
笹谷　和正 <y_r9fj22sdf@live.jp>
2016-02-23 14:32:25 +0900

    ボタン配置調整

--- AinuMonyouApp/Assets/test/json2/ScrollController.cs
+++ AinuMonyouApp/Assets/test/json2/ScrollController.cs
@@ -4,13 +4,15 @@ using UnityEngine.UI;
 using System.IO;
 using System.Linq;
 public class ScrollController : MonoBehaviour {
-
+	public Image deleteButton;
+	private bool deleteOrEdit;
 	[SerializeField]
 	RectTransform prefab;
 	private DirectoryInfo dir;
 	private FileInfo[] info;
 
 	void Start () {
+		deleteOrEdit = true;
 		/*for (int i = 0; i < 15; i++) {
 			var item = GameObject.Instantiate (prefab) as RectTransform;
 			item.SetParent (transform, false);
@@ -34,5 +36,15 @@ public class ScrollController : MonoBehaviour {
 			titleText.text = f.Name;
 			yield return null;
 		}
+	}
+
+	public void DeleteEnterButton(){
+		if (deleteOrEdit) {
+			deleteButton.color = new Color (0, 96, 96);
+
+		} else {
+			deleteButton.color = new Color (255f/255f, 96f/255f, 96f/255f);
+		}
+		deleteOrEdit = !deleteOrEdit;
 	}
 }
