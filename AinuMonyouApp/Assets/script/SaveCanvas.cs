03f7cf1bb34f130d80ea3a1e011ec7fbb1520de2
YuuyaObayashi <deziyuu@live.jp>
2016-02-23 13:34:36 +0900

    スクリーンショット多分完成 シーン結合 UI変更

--- AinuMonyouApp/Assets/script/SaveCanvas.cs
+++ AinuMonyouApp/Assets/script/SaveCanvas.cs
@@ -2,8 +2,12 @@
 using UnityEngine.UI;
 using System.Collections;
 using System.IO;
+using System.Collections.Generic;
+using System.Threading;
+using System;
 
-public class SaveCanvas : MonoBehaviour {
+public class SaveCanvas : MonoBehaviour
+{
     [SerializeField]
     private GameObject canvasObj;
     [SerializeField]
@@ -21,30 +25,35 @@ public class SaveCanvas : MonoBehaviour {
     [SerializeField]
     private GameObject inputfieldObj;
     [SerializeField]
-    private GameObject TextObj;  
+    private GameObject TextObj;
 
 
     private string path = "";
     private string fileName = "screenshot";
-    public Text text;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
-    public void checkName(){
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void checkName()
+    {
         fileName = inputfield.text;
-        if(fileName == ""){
+        if (fileName == "")
+        {
             fileName = "screenshot";
         }
 
-        if (System.IO.File.Exists(fileName+".png")){
+        if (System.IO.File.Exists(Application.persistentDataPath + "/" + fileName + ".png"))
+        {
             Debug.Log("上書き");
             inputfieldObj.SetActive(false);
             TextObj.SetActive(true);
@@ -52,13 +61,16 @@ public class SaveCanvas : MonoBehaviour {
             saveButtonObj2.SetActive(true);
             cancelButtonObj1.SetActive(false);
             cancelButtonObj2.SetActive(true);
-        } else {
+        }
+        else {
             screenshot();
         }
 
     }
 
-    public void saveButton(){
+    public void saveButton()
+    {
+        fileName = inputfield.text;
         screenshot();
     }
 
@@ -73,10 +85,13 @@ public class SaveCanvas : MonoBehaviour {
         inputfield.text = "";
     }
 
-    void screenshot() {
+    void screenshot()
+    {
         saveCanvasObj.GetComponent<Canvas>().enabled = false;
-        Application.CaptureScreenshot("storage/emulated/0/Pictures/Screenshots/"+fileName + ".png");
+        Application.CaptureScreenshot(fileName + ".png");
+        
 
+        /**
         switch (Application.platform)
         {
             case RuntimePlatform.IPhonePlayer:
@@ -84,6 +99,7 @@ public class SaveCanvas : MonoBehaviour {
                 break;
 
             case RuntimePlatform.Android:
+                CaptureScreen(this);
                 path = Application.persistentDataPath + "/" + fileName + ".png";
                 break;
 
@@ -91,21 +107,281 @@ public class SaveCanvas : MonoBehaviour {
                 path = "/" + fileName + ".png";
                 break;
         }
+        **/
 
         Debug.Log("path:" + path);
-        saveCanvasObj.GetComponent<Canvas>().enabled = true;
-        text.text = path;
-
-        
-
-        //byte[] image = File.ReadAllBytes(path);
-
-        //Texture2D tex = new Texture2D(0, 0);
-        //tex.LoadImage(image);
-
-
-
-        //canvasObj.SetActive(true);
-        //saveCanvasObj.GetComponent<Canvas>().enabled = false;
+        //saveCanvasObj.GetComponent<Canvas>().enabled = true;
+        //text.text = path;
+        Invoke("end", 0.5f);
     }
+
+    void end()
+    {
+        TextObj.SetActive(false);
+        inputfieldObj.SetActive(true);
+        saveButtonObj1.SetActive(true);
+        saveButtonObj2.SetActive(false);
+        cancelButtonObj1.SetActive(true);
+        cancelButtonObj2.SetActive(false);
+        inputfield.text = "";
+        canvasObj.SetActive(true);
+        saveCanvasObj.GetComponent<Canvas>().enabled = false;
+    }
+
+
+
+    //コルーチン
+
+    public class ScreenCapture
+    {
+        //--- クラス宣言 ---
+        private class CaptureInfo
+        {
+
+            private string path;        // パス.
+            private string id;          // 識別子( YY-MM-DD-HH-MM-SS の形式の日付文字列 ).
+            private bool isFinished;    // 画像保存完了した否か.
+            private byte[] byteData;        // キャプチャ画像のバイトデータ.
+
+            public string Path { get { return this.path; } }
+            public string Id { get { return this.id; } }
+            public bool IsFinished { get { return this.isFinished; } }
+            public byte[] ByteData { get { return this.byteData; } }
+
+            public CaptureInfo(
+                            string path,
+                            string id,
+                            byte[] byteData)
+            {
+                this.path = path;
+                this.id = id;
+                this.byteData = byteData;
+                this.isFinished = false;
+            }
+
+            public void finish()
+            {
+                this.isFinished = true;
+            }
+
+        }
+
+        //--- 定数 ---
+        private const string PICTURE_EXTENSION = ".png";
+
+        //--- 変数&プロパティ ---
+        public delegate void CallBackTex2D(Texture2D tex, string filePath);   // コールバック( Texture2D ver ).
+        public delegate void CallBackSprite(Sprite spr, string filePath);             // コールバック( Sprite ver ).
+        private Dictionary<string, CaptureInfo> captureList;                  // キャプチャ情報リスト.
+
+
+        //--- メソッド ---
+
+        // コンストラクタ.
+        public ScreenCapture()
+        {
+            this.captureList = new Dictionary<string, CaptureInfo>();
+        }
+
+
+
+        // スクリーンショットを撮影し,コールバクにSpriteと保存先パスを返す.
+        // 保存先フォルダが存在しなければ新たに作成,フォルダの指定が無ければ[プロダクト名Images]フォルダを作成.
+        // 同名ファイルがある場合に上書きするかも聞く.
+        public void captureScreenshot(
+                        MonoBehaviour mono,
+                        CallBackSprite callback,
+                        string fileName,
+                        string folderName = "",
+                        bool fileOverWrite = true)
+        {
+
+            mono.StartCoroutine(captureScreenshotCor(callback, fileName, folderName, fileOverWrite));
+        }
+        private IEnumerator captureScreenshotCor(
+                        CallBackSprite callback,
+                        string fileName,
+                        string folderName,
+                        bool fileOverWrite)
+        {
+
+            string folderPath = this.getFolderPath(folderName);
+#if UNITY_IOS
+                // iOSだとiCloudにバックアップを取りすぎるとリジェクトされる為,バックアップ属性指定フォルダのバックアップ属性を外す.
+                Device.SetNoBackupFlag( folderName );
+#endif
+            fileName += PICTURE_EXTENSION;
+            string filePath = folderPath + fileName;
+
+            yield return new WaitForEndOfFrame();
+            // 撮影.
+            Texture2D tex = new Texture2D(Screen.width, Screen.height, TextureFormat.ARGB32, false);
+            tex.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0, false);
+            tex.Apply();
+
+            // キャプチャ用の情報を作成.
+            string captureID = this.getNowTimeString();
+            CaptureInfo info = new CaptureInfo(filePath, captureID, tex.EncodeToPNG());
+            this.captureList.Add(captureID, info);
+
+            // コールバックでスプライトを先に渡す.
+            Sprite spr = Sprite.Create(tex, new Rect(0, 0, Screen.width, Screen.height), Vector2.zero);
+            callback(spr, filePath);
+
+            // 別スレッドを作成しそっちで保存.
+            Thread thread = new Thread(new ParameterizedThreadStart(this.saveCaptureImageThd));
+            thread.Start(captureID);
+            while (!this.captureList[captureID].IsFinished)
+            {
+                yield return new WaitForSeconds(0);
+            }
+
+            yield return null;
+        }
+
+
+        // スクリーンショットを撮影し,コールバックでTexture2Dとファイルパスを返す.
+        public void captureScreenshot(
+                        MonoBehaviour mono,
+                        CallBackTex2D callback,
+                        string fileName,
+                        string folderPath = "",
+                        bool isOverWrite = true)
+        {
+
+            mono.StartCoroutine(captureScreenshotCor(callback, fileName, folderPath, isOverWrite));
+        }
+        private IEnumerator captureScreenshotCor(
+                        CallBackTex2D callback,
+                        string fileName,
+                        string folderName,
+                        bool isOverWrite)
+        {
+
+            string folderPath = this.getFolderPath(folderName);
+#if UNITY_IOS
+                // iOSだとiCloudにバックアップを取りすぎるとリジェクトされる為,バックアップ属性指定フォルダのバックアップ属性を外す.
+                Device.SetNoBackupFlag( folderName );
+#endif
+            fileName += PICTURE_EXTENSION;
+            string filePath = folderPath + fileName;
+
+            yield return new WaitForEndOfFrame();
+            // 撮影.
+            Texture2D tex = new Texture2D(Screen.width, Screen.height, TextureFormat.ARGB32, false);
+            tex.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0, false);
+            tex.Apply();
+
+            // キャプチャ用の情報を作成.
+            string captureID = this.getNowTimeString();
+            CaptureInfo info = new CaptureInfo(filePath, captureID, tex.EncodeToPNG());
+            this.captureList.Add(captureID, info);
+
+            // コールバックでスプライトを先に渡す.
+            callback(tex, filePath);
+
+            // 別スレッドを作成しそっちで保存.
+            Thread thread = new Thread(new ParameterizedThreadStart(this.saveCaptureImageThd));
+            thread.Start(captureID);
+            while (!this.captureList[captureID].IsFinished)
+            {
+                yield return new WaitForSeconds(0);
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+
+
+        // フォルダの有無をチェック.
+        private string getFolderPath(
+                string folderName,
+                bool create = true)
+        {
+
+            // 出力パス( 指定したフォルダが無ければ作成 ).
+            folderName = (folderName != "") ? folderName + "/" : Application.productName + "Images/";
+            string folderPath = string.Empty;
+            if (Application.platform != RuntimePlatform.WindowsEditor)
+                folderPath = Application.persistentDataPath + "/" + folderName; // 最終的な保存先.
+            else
+                folderPath = Application.dataPath + "/" + folderName; // エディタの場合は探すのが面倒なのでプロジェクトフォルダに.
+
+            if (!Directory.Exists(folderPath) && create)
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+
+            return folderPath;
+        }
+
+
+
+        // 指定したパスのバイナリデータをbyte型配列で返す.
+        private byte[] readBinary(
+                        string path)
+        {
+
+            byte[] bytes = (File.Exists(path)) ? File.ReadAllBytes(path) : null;
+            if (bytes == null)
+                Debug.LogError("ファイルが存在しません.");
+            return bytes;
+        }
+
+
+        // 指定したパス,サイズのTexture2Dを作成し,返す.
+        private Texture2D readTexture2D(
+                        string path,
+                        int width,
+                        int height)
+        {
+
+            byte[] data = this.readBinary(path);
+            Texture2D tex = new Texture2D(width, height);
+            tex.LoadImage(data);
+
+            return tex;
+        }
+
+
+        // 指定したパス,サイズのスプライトを作成し,返す.
+        private Sprite readSprite(
+                        string path,
+                        int width,
+                        int height)
+        {
+
+            Texture2D tex = this.readTexture2D(path, width, height);
+            Sprite spr = Sprite.Create(tex, new Rect(0, 0, width, height), Vector2.zero);
+
+            return spr;
+        }
+
+
+        // 現在時刻のストリングを取得.
+        // フォーマット : YY-MM-DD-OO-MM-SS.
+        private string getNowTimeString()
+        {
+
+            string ret = DateTime.Now.Year.ToString() + "-";
+            ret += DateTime.Now.Month.ToString() + "-";
+            ret += DateTime.Now.Day.ToString() + "-";
+            ret += DateTime.Now.Hour.ToString() + "-";
+            ret += DateTime.Now.Minute.ToString() + "-";
+            ret += DateTime.Now.Second.ToString();
+            return ret;
+        }
+
+
+        // 別スレッド用メソッド.
+        // 指定されたidのキャプチャ情報を保存.
+        private void saveCaptureImageThd(
+                        object id)
+        {
+
+            string captureID = (string)id;
+            File.WriteAllBytes(this.captureList[captureID].Path, this.captureList[captureID].ByteData);
+            this.captureList[captureID].finish();
+        }
+    }
+
 }
---
+++ AinuMonyouApp/Assets/script/title.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class title : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Application.platform == RuntimePlatform.Android){
+            if(Input.GetKey(KeyCode.Home)|| Input.GetKey(KeyCode.Escape) || Input.GetKey(KeyCode.Menu))
+            {
+                Application.Quit();
+                return;
+            }
+        }
+	}
+
+    public void makeButton()
+    {
+        Application.LoadLevel("make");
+    }
+
+    public void showButton()
+    {
+        Application.LoadLevel("show");
+    }
+}
